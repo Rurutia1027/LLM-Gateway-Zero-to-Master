592c814d97b4abaae7b9b0c497221ecb58ed8404
Rurutia1027 <rurutia1027@gmail.com>
2026-07-22 01:33:10 +0800

    add signed key , schema, issue key and middleware impl

--- chapter04/src/db/schema.ts
+++ chapter04/src/db/schema.ts
@@ -16,7 +16,7 @@ export const orgs = sqliteTable('orgs', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
   // null = enabled; non-null = disabled timestamp
-  disableAt: integer('disable_at'),
+  disabledAt: integer('disable_at'),
   createdAt: integer('created_at').notNull(),
 });
 
